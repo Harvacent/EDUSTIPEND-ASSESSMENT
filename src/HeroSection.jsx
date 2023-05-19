7775fdfb4149d9c63069b8e10823784cc038a1d3
Harvacent <johnnexdee@gmail.com>
2023-05-19 14:36:49 -0900

    submission

---
+++ src/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+
+export default function HeroSection() {
+  return (
+    <div className='hero'>
+        <h1 className='h1'>Make your party fun!</h1>
+        <p className='p'>Create your own custom playlist today.</p>
+        <button className='btn3'>Create playlist</button>
+    </div>
+  )
+}
---
+++ src/TopBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles.css'
+
+
+export default function TopBar() {
+  return (
+    <div className='top'>
+        <div className='buttons'>
+            <button className='btn1'>Contact us</button>
+            <button className='btn2'>Sign up</button>
+        </div>
+    </div>
+  )
+}
